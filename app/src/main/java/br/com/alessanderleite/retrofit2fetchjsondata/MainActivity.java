5f64bc5fbc01559a6f5e03fd0482faa8d4ff6f9d
alessanderleite <alessander.logistica@gmail.com>
2019-04-13 23:52:40 -0300

    declare the recyclerview and retrofit inside main activity

--- app/src/main/java/br/com/alessanderleite/retrofit2fetchjsondata/MainActivity.java
+++ app/src/main/java/br/com/alessanderleite/retrofit2fetchjsondata/MainActivity.java
@@ -2,12 +2,64 @@ package br.com.alessanderleite.retrofit2fetchjsondata;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getItemsApi();
+    }
+
+    private void getItemsApi() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Api.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        Api api = retrofit.create(Api.class);
+        Call<List<Item>> call = api.getItems();
+        call.enqueue(new Callback<List<Item>>() {
+            @Override
+            public void onResponse(Call<List<Item>> call, Response<List<Item>> response) {
+                List<Item> itemList = response.body();
+                String[] items = new String[itemList.size()];
+                for (int i = 0; i < itemList.size(); i++) {
+                    items[i] = itemList.get(i).getName();
+                }
+
+                mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+                mRecyclerView.setHasFixedSize(true);
+                mLayoutManager = new LinearLayoutManager(MainActivity.this);
+                mRecyclerView.setLayoutManager(mLayoutManager);
+                mAdapter = new Adapter((ArrayList<Item>) itemList);
+                mRecyclerView.setAdapter(mAdapter);
+            }
+
+
+
+            @Override
+            public void onFailure(Call<List<Item>> call, Throwable t) {
+
+            }
+        });
     }
 }
