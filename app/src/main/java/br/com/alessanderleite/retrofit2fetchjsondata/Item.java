300051921ce9f929eb49b6aab4a130d612f337f6
alessanderleite <alessander.logistica@gmail.com>
2019-04-13 21:45:10 -0300

    create the class Item to represent the model

---
+++ app/src/main/java/br/com/alessanderleite/retrofit2fetchjsondata/Item.java
@@ -0,0 +1,14 @@
+package br.com.alessanderleite.retrofit2fetchjsondata;
+
+public class Item {
+
+    private String name;
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
