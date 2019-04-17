5a0501869a5cc18ad3b9218b9769396df7734e53
alessanderleite <alessander.logistica@gmail.com>
2019-04-16 21:58:25 -0300

    add Annotation @SerializedName for each constant at Item.java class

--- app/src/main/java/br/com/alessanderleite/retrofit2fetchjsondata/model/Item.java
+++ app/src/main/java/br/com/alessanderleite/retrofit2fetchjsondata/model/Item.java
@@ -1,28 +1,49 @@
 package br.com.alessanderleite.retrofit2fetchjsondata.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class Item {
 
-    private String name;
-    private String email;
+    @SerializedName("login")
+    @Expose
+    private String login;
 
-    public Item(String name, String email) {
-        this.name = name;
-        this.email = email;
+    @SerializedName("avatar_url")
+    @Expose
+    private String avatarUrl;
+
+    @SerializedName("html_url")
+    @Expose
+    private String html_url;
+
+    public Item(String login, String avatarUrl, String html_url) {
+        this.login = login;
+        this.avatarUrl = avatarUrl;
+        this.html_url = html_url;
     }
 
-    public String getName() {
-        return name;
+    public String getLogin() {
+        return login;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setLogin(String login) {
+        this.login = login;
     }
 
-    public String getEmail() {
-        return email;
+    public String getAvatarUrl() {
+        return avatarUrl;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getHtml_url() {
+        return html_url;
+    }
+
+    public void setHtml_url(String html_url) {
+        this.html_url = html_url;
     }
 }
