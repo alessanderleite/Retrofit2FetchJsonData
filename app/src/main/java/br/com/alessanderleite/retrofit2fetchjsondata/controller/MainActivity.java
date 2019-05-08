11a0d2c13f6010ed1979a34a228af3fa4808d529
alessanderleite <alessander.logistica@gmail.com>
2019-05-08 14:25:20 -0300

    add the SearchView object inside MainActivity

--- app/src/main/java/br/com/alessanderleite/retrofit2fetchjsondata/controller/MainActivity.java
+++ app/src/main/java/br/com/alessanderleite/retrofit2fetchjsondata/controller/MainActivity.java
@@ -4,10 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Toast;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import br.com.alessanderleite.retrofit2fetchjsondata.adapter.ItemAdapter;
 import br.com.alessanderleite.retrofit2fetchjsondata.api.Service;
@@ -22,8 +27,9 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private ItemAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private List<Item> itemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +46,8 @@ public class MainActivity extends AppCompatActivity {
             call.enqueue(new Callback<ItemResponse>() {
                 @Override
                 public void onResponse(Call<ItemResponse> call, Response<ItemResponse> response) {
-                    getItemList(response.body().getItemArrayList());
+                    itemList = response.body().getItemArrayList();
+                    getRecyclerViewInstance();
                 }
 
                 @Override
@@ -54,14 +61,37 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void getItemList(ArrayList<Item> itemArrayList) {
+    public void getRecyclerViewInstance() {
 
             mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
             mRecyclerView.setHasFixedSize(true);
             mLayoutManager = new LinearLayoutManager(MainActivity.this);
             mRecyclerView.setLayoutManager(mLayoutManager);
-            mAdapter = new ItemAdapter(itemArrayList,this);
+            mAdapter = new ItemAdapter(itemList,this);
             mRecyclerView.setAdapter(mAdapter);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_section);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                mAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return true;
     }
 }
